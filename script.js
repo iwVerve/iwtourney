9f01b514e5792dc0a3d59e564eac09e930042c39
iwVerve <thedolphiner@gmail.com>
2021-11-27 18:14:33 +0100

    replace ship dl

--- script.js
+++ script.js
@@ -492,7 +492,7 @@ $ (document).ready(function() {
             maker: 'Artimax, AlejoFangamer, RandomErik, Princeoflight, Smartkin, Chrisay, EchoMask',
             desc: 'Ranking criteria: Fewest deaths',
             img: 'img/game/savetheship.jpg',
-            link: 'https://www.mediafire.com/folder/db61gewsp9ls8/I_Wanna_Save_the_Ship',
+            link: 'https://www.mediafire.com/file/ojxrnaxc9cqpar7/I+Wanna+Save+the+Ship.zip/file',
             ranking: []
         }
     }
